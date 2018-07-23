7096a7a257a79e559c4fae22301e4a91529e1e76
Mangla75441 <manglaverma0@gmail.com>
2018-07-23 18:25:59 +0530

    product syncing to rails app

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,7 +5,8 @@ Spree::Product.class_eval do
 			p 'SYNC APFUSION'
 			p '.'*50
 			p self.as_json
-			p SpreeApfusion::Product.create(self.as_json)
+			p SpreeApfusion::Product.create(self)
+			# p SpreeApfusion::Image.create()
 			p '.'*50
 		end
 	end
--- lib/spree_apfusion/products.rb
+++ lib/spree_apfusion/products.rb
@@ -2,12 +2,34 @@ module SpreeApfusion
   class Product
 
   	def self.create product
-  		SpreeApfusion::OAuth.send(:post, '/api/v1/products.json', {product: SpreeApfusion::Product.sanitize_params(product)})
+      @product = product
+      @product_hash 
+  		# p "===#{product}==productCALL==="
+  		# p "========#{SpreeApfusion::Product.sanitize_params(product)}=============sanitizeparams=========="
+      SpreeApfusion::Product.generate_product_hash 
+  		SpreeApfusion::OAuth.send(:post, '/api/v1/products.json', {product: @product_hash})
   	end
 
-  	def self.sanitize_params product
-  		product.slice(:name)
-  	end
+  	# def self.sanitize_params product
+  	# 	p "==========#{product}==sanitizeFunction===="
+  	# 	product.except(:name,:price, :shipping_category)
+  	# end
+
+    def self.generate_product_hash 
+      SpreeApfusion::Product.add_product_price
+    end
+
+
+    def self.add_product_price 
+      p "++++++++++++++++#{@product}++++++++++++++++++"
+      p "-----------------#{@product.price}-----"
+      @product_hash = @product.attributes
+      @product_hash["price"] = @product.price
+
+    end
 
 	end
 end
+
+
+# SpreeApfusion::OAuth.send(:post, '/api/v1/products.json', {product: {name:'test product', price: 12,shipping_category: 'default'}})
