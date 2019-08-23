cfe210191a2aabee695cbb3329786116d87be2b7
Mangla75441 <manglaverma0@gmail.com>
2019-08-23 12:14:16 +0530

    stock item create one condition added

--- lib/spree_apfusion/stock_item.rb
+++ lib/spree_apfusion/stock_item.rb
@@ -6,8 +6,12 @@ module SpreeApfusion
       @stock_item_hash 
       SpreeApfusion::StockItem.generate_stock_item_hash 
       response = SpreeApfusion::OAuth.send(:post, '/api/v2/stock_locations/'+@stock_item.stock_location.apfusion_stock_location_id.to_s+'/stock_items.json', {stock_item: @stock_item_hash})
-      if response[:success] == true && response[:response].present? && response[:response]["id"].present?                
-         @stock_item.update_attributes(apfusion_stock_item_id: response[:response]["id"])
+      if response[:success] == true && response[:response].present? 
+        if response[:response].is_a?(Hash) && response[:response]["id"].present?                
+           @stock_item.update_attributes(apfusion_stock_item_id: response[:response]["id"])
+        elsif response[:response].is_a?(Array)
+          @stock_item.update_attributes(apfusion_stock_item_id: response[:response][0]["id"])
+        end 
       end
     end
 
@@ -16,7 +20,7 @@ module SpreeApfusion
       @stock_item.id
       SpreeApfusion::StockItem.generate_stock_item_hash 
 
-      SpreeApfusion::OAuth.send(:PUT, '/api/v2/stock_locations/'+@stock_item.stock_location.apfusion_stock_location_id.to_s+'/stock_items/'+@stock_item.apfusion_stock_item_id.to_s+'.json', {stock_item: @stock_item_hash})
+      response = SpreeApfusion::OAuth.send(:PUT, '/api/v2/stock_locations/'+@stock_item.stock_location.apfusion_stock_location_id.to_s+'/stock_items/'+@stock_item.apfusion_stock_item_id.to_s+'.json', {stock_item: @stock_item_hash})
     end
 
 
