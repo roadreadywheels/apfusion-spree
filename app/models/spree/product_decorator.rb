d7567d232a5539a0a5ce21d1e990542da75ff903
mangla <techdev@w3villa.com>
2020-01-23 13:09:57 +0530

    product apfusion_product_id uniquensees

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,6 +3,7 @@ Spree::Product.class_eval do
 		# after_update :update_at_apfusion
 		# after_destroy :destroy_at_apfusion
 
+		validates :apfusion_product_id,:uniqueness => true
 
 		def create_at_apfusion
 			SpreeApfusion::Product.create(self)
