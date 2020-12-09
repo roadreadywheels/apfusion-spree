6287286e7be329a4843a65c4bfe5e913017c410d
Mangla Verma <manglaverma0@gmail.com>
2020-12-09 14:13:28 +0530

    code reveted to normal sycn

--- lib/spree_apfusion/order.rb
+++ lib/spree_apfusion/order.rb
@@ -7,22 +7,17 @@ module SpreeApfusion
         @order_id = @order.apfusion_order_id
       end 
       @orders_hash = []
-      # page = 1
-      # loop do
-      #   orders = SpreeApfusion::OAuth.send(:GET, '/api/v2/orders', {page: page, q: {completed_at_gt: @order.apfusion_completed_at}})[:response]
-      #   orders["orders"].each do |order|
-      ["R835428948"].each do |number|
-        # binding.pry
-        order_number = number
-        order_details = SpreeApfusion::OAuth.send(:GET, "/api/v2/orders/#{order_number}", {})[:response]
-            @orders_hash.push(order_details) 
+      page = 1
+      loop do
+        orders = SpreeApfusion::OAuth.send(:GET, '/api/v2/orders', {page: page, q: {completed_at_gt: @order.apfusion_completed_at}})[:response]
+        orders["orders"].each do |order|
+          order_details = SpreeApfusion::OAuth.send(:GET, "/api/v2/orders/#{order["number"]}", {})[:response]
+        @orders_hash.push(order_details)  
+        end
+        page = page + 1
+        sleep 5 #to prevent api limits being hit.
+        break if orders["count"] < 25
       end
- 
-        # end
-      #   page = page + 1
-      #   sleep 5 #to prevent api limits being hit.
-      #   break if orders["count"] < 25
-      # end
       return @orders_hash
     end
 
