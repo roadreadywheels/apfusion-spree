51480ed06f5a7d7c7336838dea0a7a10911603d4
w3villa-amit <amit.dhoundiyal@w3villa.com>
2022-04-08 18:53:13 +0530

    APF-309 | RRW APF orders - some orders have not synced
    make q as dynamic
    
    make q as dynamic

--- lib/spree_apfusion/order.rb
+++ lib/spree_apfusion/order.rb
@@ -1,25 +1,27 @@
 module SpreeApfusion
   class Order
 
-    def self.get_orders
+    def self.get_orders(options = {})
       @order = Spree::Order.where.not(apfusion_order_id: nil).order(:created_at).last
       if @order.present?
         @order_id = @order.apfusion_order_id
       end 
-      @orders_hash = []
-      page = 1
+      @orders = []
+      page = options[:page] || 1
+      per_page = options[:per_page] || 10
+      query = options[:q] || {}
+      query[:completed_at_gt] = @order.apfusion_completed_at
       loop do
-        orders = SpreeApfusion::OAuth.send(:GET, '/api/v2/orders', {page: page, q: {completed_at_gt: @order.apfusion_completed_at}})[:response]
+        orders = SpreeApfusion::OAuth.send(:GET, '/api/v2/orders', {page: page, per_page: per_page, q: query})[:response]
         orders["orders"].each do |order|
           order_details = SpreeApfusion::OAuth.send(:GET, "/api/v2/orders/#{order["number"]}", {})[:response]
-        @orders_hash.push(order_details)  
+        @orders.push(order_details)  
         end
         page = page + 1
         sleep 5 #to prevent api limits being hit.
-        break if orders["count"] < 25
+        break if orders["count"] < per_page
       end
-      return @orders_hash
+      return @orders
     end
-
   end 
 end
