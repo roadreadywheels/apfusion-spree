d114b7dffd8c0954e9022eec9e4294ed76156eb7
Harshita Gupta <harshita.gupta@w3villa.com>
2022-04-28 00:52:56 +0530

    RRW-84 | APFusion is overriding address in the RRW

---
+++ app/models/concerns/spree/apfusion_order_concern.rb
@@ -0,0 +1,163 @@
+module Spree
+  module ApfusionOrderConcern
+    extend ActiveSupport::Concern
+
+    APFUSION_PAYMENT_METHOD_NAME = 'Create at Apfusion'.freeze
+    APFUSION_PAYMENT_METHOD = {
+          'type'=>'Spree::PaymentMethod::Check',
+          'display_on'=>'both',
+          'auto_capture'=>'',
+          'active'=>'true',
+          'name'=> APFUSION_PAYMENT_METHOD_NAME,
+          'description'=>'Payment for Apfusion app'}.freeze
+
+    def self.get_apfusion_response options
+      return SpreeApfusion::Order.get_orders if options.blank?
+
+      SpreeApfusion::Order.get_specific_orders(options)
+    end
+
+    def self.create_apfusion_orders response, order_collection = []
+      response.each do |order|
+        apf_order = Spree::Order.find_by_apfusion_order_id(order['id']) || self.create_apfusion_order(order)
+        order_collection.push(apf_order) if apf_order.present?
+      end
+      return order_collection
+    end
+
+    def self.create_apfusion_order order
+      begin
+        @order = Spree::Order.new
+        orders_attributes = {
+          'email' => order['user_email'],
+          'special_instructions' => order['special_instructions'],
+          'apfusion_order_id' => order['id'],
+          'apfusion_completed_at' => order['completed_at'],
+          'user_id' => order['rrw_user_id']
+        }
+        @order.update(orders_attributes)
+        @order.set_methods(order)
+        return @order
+      rescue Exception => e
+        self.send_exception(e, order)
+        e.message
+      end
+    end
+
+    def set_methods order
+      apfusion_update_addresses(order)
+      apfusion_set_line_item_shipments(order)
+      apfusion_create_payment
+      adjustments.destroy_all
+      reload_totals
+    end
+
+    def reload_totals
+      update_totals
+      persist_totals
+      self.next
+      self.next
+    end
+
+    def apfusion_update_addresses order
+      self.temporary_address = true
+      orders_attributes = {
+        'bill_address_attributes' => apfusion_set_address(order['bill_address']),
+        'ship_address_attributes' => apfusion_set_address(order['ship_address'])
+      }
+      update(orders_attributes)
+      self.next
+    end
+
+    def apfusion_set_line_item_shipments order
+      order['line_items'].delete_if { |k| k.empty? }
+      order['shipments'].delete_if { |k| k.empty? }
+      self.apfusion_create_line_item_shipments(order['shipments'], order['line_items'])
+    end
+
+    def apfusion_create_line_item_shipments shipments, line_items
+      shipments.each do |shipment|
+        shipment_record = self.apfusion_create_shipments(shipment)
+
+        shipment['manifest'].each do |manifest|
+          line_items.each do |line_item|
+            if line_item['variant_id'] == manifest['variant_id']
+              variant = line_item['variant']['is_master'] ?
+                        Spree::Product.find_by_apfusion_product_id(line_item['source_id']).master :
+                        Spree::Variant.find_by_apfusion_variant_id(line_item['source_id'])
+              self.contents.add(variant, manifest['quantity'], {shipment: shipment_record}, (line_item['price'] || 0))
+            end
+          end
+        end
+      end
+    end
+
+    def apfusion_create_shipments shipment
+      shipping_method_name = shipment['shiping_method_name']['linked_from']
+      shipping_method = Spree::ShippingMethod.find_by_name(shipping_method_name)
+      stock_location = Spree::StockLocation.find_by_name(shipment['stock_location_name'])
+      shipment_record = self.shipments.create(stock_location_id: stock_location.id, apfusion_shipment_id: shipment['number'])
+      shipment_record.shipping_rates.create(shipping_method: shipping_method, selected: true, cost: shipment["cost"])
+      return shipment_record
+    end
+
+    def apfusion_delete_fields address
+      ['id', 'email', 'customer_number', 'full_name', 'state_text', 'country', 'state'].map{ |field| 
+        address.delete(field)
+      }
+      return address
+    end
+
+    def apfusion_set_address address
+      country = Spree::Country.find_by_name(address['country']['name']) rescue ''
+      address['country_id'] = country.present? ? country.id : address['country_id']
+      address['state_id'] = country.present? ? country.states.find_by_name(address['state']['name']).id : address['state_id']
+      address['phone'] = '888-790-5899' if !address['phone'].present?
+      apfusion_delete_fields(address)
+      return address
+    end
+
+    def apfusion_get_address address, allow_override = true
+      return apfusion_set_address(address) if allow_override
+
+      Spree::Address.create(apfusion_set_address(address)).id
+    end
+
+    def apfusion_create_payment
+      reload_totals
+      payments.create(amount: total, payment_method_id: apfusion_payment_method.id)
+    end
+
+    def apfusion_payment_method
+      check_payment_method = Spree::PaymentMethod.find_by_name(APFUSION_PAYMENT_METHOD_NAME)
+      if check_payment_method.present?
+        check_payment_method
+      else
+        Spree::PaymentMethod.create(APFUSION_PAYMENT_METHOD)
+      end
+    end
+
+    def self.apfusion_exception_data order, error
+      return [{key: 'APFusion Order', value: order['number']},
+      {key: 'Email', value: order['email']},
+      {key: 'State', value: order['state']},
+      {key: 'Total', value: order['total']},
+      {key: 'Payment State', value: order['payment_state']},
+      {key: 'Shipment State', value: order['shipment_state']},
+      {key: 'Special Instructions', value: order['special_instructions']},
+      {key: 'Error Logs', value: "#{error.backtrace.to_s[0..500]} ..."}]
+    end
+
+    def self.send_exception error, order
+      @order.destroy
+      err_data = {
+        message: error.message,
+        function_where_issue_occurred: 'APFusion Sync Order issue',
+        cc_email: nil,
+        other: self.apfusion_exception_data(order, error),
+        type: 'apfusion_orders_sync'
+      }
+      Spree::UserMailer.error_email(err_data).deliver!
+    end
+  end
+end
--- lib/spree_apfusion/oauth.rb
+++ lib/spree_apfusion/oauth.rb
@@ -1,144 +1,115 @@
 module SpreeApfusion
-	class OAuth
+  class OAuth
 
-		def self.init
-			# @url = 'http://apfusion.com/'
-			# 
-			urls = {
-				"development" => "http://localhost:3000/",
-				"staging" => "http://34.217.121.110/",
-				"production" => "https://apfusion.com/"
-			}
-		 	 	@url = urls[Rails.env]
-				@grant_type = 'client_credentials'
+    def self.init
+      record = SpreeApfusion::OAuth.apfusion_token_record
+      @url = record.get_scope
+      @access_token = record.get_token if record.present?
+    end
 
-			begin
-				@apfusion_auth_config = YAML.load_file(Rails.root.to_s + '/config/apfusion_auth_config.yml')[Rails.env]
-			rescue
-				raise "
-				\n===============================================================
-				\n Please create a config file at /config/apfusion_auth_config.yml like the example given below
-				\n===============================================================
-				\ndevelopment:
-				\n\tpublic_key: PUBLIC_KEY
-				\n\tsecret_key: SECRET_KEY
-				\n===============================================================\n"
-			end
-			# @apfusion_auth_config = YAML.load_file('/Users/afzal/rails/gems/spree_apfusion/config/apfusion_auth_config.yml')[Rails.env]
-			@public_key = @apfusion_auth_config['public_key']
-			@secret_key = @apfusion_auth_config['secret_key']
-			@access_token = ApfusionToken.first.try(:token) || ''
-		end
+    def self.get_access_token
+      self.initialize_config_elements
+      begin
+        request = RestClient.post("#{@url}/oauth/token", {client_id: @public_key, client_secret: @secret_key, grant_type: self.grant_type})
+        @access_token = JSON.parse(request.body)['access_token']
+        SpreeApfusion::OAuth.update_token
+        return {success: true}
+      rescue => e
+        error = e.as_json
+        if error.is_a? String
+          return {success: false, response: "#{error} - In getting access token."}
+        else
+          return {success: false, response: JSON.parse(error['response'].body), response_code: error['initial_response_code']}
+        end
+      end
+    end
 
-		def self.get_access_token
-			begin
-				request = RestClient.post(@url+'/oauth/token', {client_id: @public_key, client_secret: @secret_key, grant_type: @grant_type})
-				@access_token = JSON.parse(request.body)['access_token']
+    def self.update_token
+      ApfusionToken.first_or_create.update_attributes(scope: @url, token: @access_token)
+    end
 
-				SpreeApfusion::OAuth.update_token
+    def self.authorize
+      SpreeApfusion::OAuth.init
 
-				return {success: true}
+      if @access_token.blank?
+        response = SpreeApfusion::OAuth.get_access_token
+        unless response[:success]
+          return response
+        end
+      end
 
-			rescue => e
-				error = e.as_json
-				if error.is_a? String
-					return {success: false, response: "#{error} - In getting access token."}
-				else
-					return {success: false, response: JSON.parse(error['response'].body), response_code: error['initial_response_code']}
-				end
-			end
-		end
+      { status: 'Authorized!', access_token: @access_token }
+    end
 
-		def self.update_token
-			if ApfusionToken.first.present?
-				ApfusionToken.first.update_attributes(token: @access_token)
-			else
-				ApfusionToken.create(token: @access_token)
-			end
-		end
+    def self.send method, url_path, data
+      p '========================'
+      p "Method: #{method}"
+      p "Url: #{url_path}"
+      p "Params: #{data}"
+      p '========================'
+      SpreeApfusion::OAuth.authorize
+      p 'after authorize called'
+      request = self.client(url_path: url_path, method: method, data: data)
 
-		def self.authorize
-			SpreeApfusion::OAuth.init
-			if @access_token.blank?
-				response = SpreeApfusion::OAuth.get_access_token
-				unless response[:success]
-					return response
-				end
-			end
+      begin
+        response = request.execute {|response| $results = response}
+        response_body = JSON.parse(response.body) if response.present?
+        case response.code.to_s
+        when /^20/
+          p 'SYNC Successfully'
+          return self.get_response(success: true, response_body: response_body, code: response.code)
+        when '401'
+          p '!!!!Invalid Access Token!!!!'
+          ApfusionToken.destroy_all
+          SpreeApfusion::OAuth.send(method, url_path, data)
+        else
+          return self.get_response(success: false, response_body: response_body, code: response.code)
+        end
+      rescue Exception => e
+        p e.message
+      end
+    end
 
-			{status: 'Authorized!', access_token: @access_token}
-		end
+    def self.get_response success: false, response_body:, code:
+      {
+        success: success,
+        response: response_body,
+        response_code: code
+      }
+    end
 
-		def self.send (method, url_path	, data)
-			p '========================'
-			p "Method: #{method}"
-			p "Url: #{url_path}"
-			p "Params: #{data}"
-			p '========================'
-			SpreeApfusion::OAuth.authorize
-			p "after authorize called"
-			request = RestClient::Request.new(
-				method: method,
-				url: @url+url_path+'?access_token='+@access_token,
-				headers: {params: data}
-			)
-			begin
-			response = request.execute {|response| $results = response}
+    def self.client url_path:, method:, data:
+      RestClient::Request.new(
+        method: method,
+        url: "#{@url}#{url_path}?access_token=#{@access_token}",
+        headers: { params: data }
+        )
+    end
 
-			response.body
-			
-				response_body = JSON.parse(response.body)
-			rescue
-				response_body = ''
-			end
+    def self.apfusion_token_record
+      ApfusionToken.first_or_create
+    end
 
-			begin
-				
-				case response.code.to_s
+    def self.grant_type
+      'client_credentials'
+    end
 
-					when /^20/
-						p 'SYNC Successfully'
-						return {success: true, response: response_body, response_code: response.code}
-					when '401'
-						p '!!!!Invalid Access Token!!!!'
-						ApfusionToken.destroy_all
-						SpreeApfusion::OAuth.send(method, url_path, data)
-					else
-						return {success: false, response: response_body, response_code: response.code}
-				end
-			rescue Exception => e
-				p e.message
-			end
-			# p 1
-			# SpreeApfusion::OAuth.authorize
-			# p 2
-			# begin
-			# 	p 3
-			# 	p method
-			# 	request = RestClient.send(method, @url+url_path+'?access_token='+@access_token, data)
-			# 	p request.as_json
-			# 	if request.present?
-			# 		return JSON.parse(request.body)
-			# 	else
-			# 		return {success: false, response: 'Error! Unable to send data.'}
-			# 	end
-
-			# rescue => e
-			# 	p 4
-			# 	error = e.as_json
-			# 	if error == "401 Unauthorized" || error['initial_response_code'] == 401
-			# 		ApfusionToken.destroy_all
-			# 		SpreeApfusion::OAuth.send(url_path, data)
-			# 	else
-			# 		if error.is_a? String
-			# 			return {success: false, response: "#{error} - In sending data."}
-			# 		else
-			# 			return {success: false, response: JSON.parse(error['response'].body), response_code: error['initial_response_code']}
-			# 		end
-			# 	end
-
-			# end
-		end
-
-	end
+    def self.initialize_config_elements
+      begin
+        @apfusion_auth_config = YAML.load_file(Rails.root.to_s + '/config/apfusion_auth_config.yml')[Rails.env]
+      rescue
+        raise "
+        \n===============================================================
+        \n Please create a config file at /config/apfusion_auth_config.yml like the example given below
+        \n===============================================================
+        \ndevelopment:
+        \n\tpublic_key: PUBLIC_KEY
+        \n\tsecret_key: SECRET_KEY
+        \n===============================================================\n"
+      end
+      # @apfusion_auth_config = YAML.load_file('/Users/afzal/rails/gems/spree_apfusion/config/apfusion_auth_config.yml')[Rails.env]
+      @public_key = @apfusion_auth_config['public_key']
+      @secret_key = @apfusion_auth_config['secret_key']
+    end
+  end
 end
