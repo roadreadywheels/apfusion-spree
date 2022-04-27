5ac41af5f6eccc19b29768b7a01b7399a1b26722
Harshita Gupta <harshita.gupta@w3villa.com>
2022-04-28 00:52:56 +0530

    RRW-84 | APFusion is overriding address in the RRW

--- lib/app/models/apfusion_token.rb
+++ lib/app/models/apfusion_token.rb
@@ -1,2 +1,14 @@
 class ApfusionToken < ActiveRecord::Base
+  def get_scope
+    urls = {
+      'development' => 'http://localhost:3000/',
+      'staging' => 'https://staging.apfusion.com/',
+      'production' => 'https://apfusion.com/'
+    }
+    try(:scope) || urls[Rails.env]
+  end
+
+  def get_token
+    try(:token) || ''
+  end
 end
