47009ebee35a6750bcc6bbfcf4999def20780e09
naiyyarabbas <naiyyar.abbas@w3villa.com>
2022-06-20 16:27:22 +0530

    code refactored

--- lib/spree_apfusion/product.rb
+++ lib/spree_apfusion/product.rb
@@ -2,61 +2,60 @@ module SpreeApfusion
   class Product
 
   	def self.create product
-      @product = product
-      @product_hash 
-      SpreeApfusion::Product.generate_product_hash 
-  		response = SpreeApfusion::OAuth.send(:post, '/api/v2/products.json', {product: @product_hash})
+      product_hash = SpreeApfusion::Product.generate_product_hash(product)
+  		response = SpreeApfusion::OAuth.send(:post, '/api/v2/products.json', {product: product_hash})
       if response[:success] == true && response[:response].present? && response[:response]["id"].present?                
-        @product.update_attributes(apfusion_product_id: response[:response]["id"])
-        @product.master.update_attributes(apfusion_variant_id: response[:response]["master"]["id"])
+        product.update_attributes(apfusion_product_id: response[:response]["id"])
+        product.master.update_attributes(apfusion_variant_id: response[:response]["master"]["id"])
       end   
   	end
 
 
     def self.update product
-      @product = product
-      @product.apfusion_product_id
-      SpreeApfusion::Product.generate_product_hash 
-      response = SpreeApfusion::OAuth.send(:PUT, '/api/v2/products/'+@product.apfusion_product_id.to_s+'.json', {product: @product_hash,filter_type: "id"}) 
+      return if product.blank?
+
+      product_hash = SpreeApfusion::Product.generate_product_hash(product)
+      response = SpreeApfusion::OAuth.send(:PUT, '/api/v2/products/'+product.apfusion_product_id.to_s+'.json', {product: product_hash,filter_type: "id"}) 
     end  
 
     def self.destroy product
-      @product = product
-      @product.id
-      SpreeApfusion::Product.generate_product_hash 
-      SpreeApfusion::OAuth.send(:DELETE, '/api/v2/products/'+@product.id.to_s+'.json', {product: @product_hash}) 
+      return if product.blank?
+
+      product_hash = SpreeApfusion::Product.generate_product_hash(product)
+      SpreeApfusion::OAuth.send(:DELETE, '/api/v2/products/'+product.id.to_s+'.json', {product: product_hash}) 
     end  
 
-    def self.generate_product_hash 
-      @product_hash = @product.attributes
-      SpreeApfusion::Product.add_product_price
-      SpreeApfusion::Product.add_option_type_id
-      SpreeApfusion::Product.add_sku
-      SpreeApfusion::Product.add_taxons
+    def self.generate_product_hash product = nil
+      return {} if product.blank?
 
+      @product_hash = product.attributes
+      SpreeApfusion::Product.add_product_price(product)
+      SpreeApfusion::Product.add_option_type_id(product)
+      SpreeApfusion::Product.add_sku(product)
+      SpreeApfusion::Product.add_taxons(product)
+      @product_hash
     end
 
 
-    def self.add_product_price 
-      if @product.apfusion_amount.present?
-        @product_hash["price"] = @product.apfusion_amount
+    def self.add_product_price product
+      if product.apfusion_amount.present? && product.apfusion_amount.to_i > 0
+        @product_hash["price"] = product.apfusion_amount
       else
-        @product_hash["price"] = @product.price + (@product.price * 0.08)
+        @product_hash["price"] = product.price + (product.price * 0.08)
       end
-      @product_hash["resale_amount"] = @product.resale_amount
-      @product_hash["bsap_amount"] = @product.bsap_amount + (@product.bsap_amount * 0.09)
-
+      @product_hash["resale_amount"] = product.resale_amount
+      @product_hash["bsap_amount"] = product.bsap_amount + (product.bsap_amount * 0.09)
     end
 
-    def self.add_option_type_id
-      @product_hash["option_type_ids"] = @product.option_types.collect(&:apfusion_option_type_id)
+    def self.add_option_type_id product
+      @product_hash["option_type_ids"] = product.option_types.collect(&:apfusion_option_type_id)
     end
 
-    def self.add_sku
-      @product_hash["sku"] = @product.sku
+    def self.add_sku product
+      @product_hash["sku"] = product.try(:sku)
     end
 
-    def self.add_taxons
+    def self.add_taxons product
        @product_hash["taxon_ids"] = "5"
     end
 
