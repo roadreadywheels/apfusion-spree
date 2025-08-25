4db2b49cfec42386a86efb0d6566a76d3dddb117
Harshita Gupta <harshita.gupta@w3villa.com>
2025-08-25 14:31:23 +0530

    Added response migration to gem

--- db/migrate/20200608142558_add_updated_at_in_spree_prices.rb
+++ db/migrate/20200608142558_add_updated_at_in_spree_prices.rb
@@ -1,6 +1,6 @@
 class AddUpdatedAtInSpreePrices < ActiveRecord::Migration
   def change
-  	add_column :spree_prices, :created_at, :datetime, null: false
-    add_column :spree_prices, :updated_at, :datetime, null: false
+  	add_column :spree_prices, :created_at, :datetime, null: false unless column_exists?(:spree_prices, :created_at)
+    add_column :spree_prices, :updated_at, :datetime, null: false unless column_exists?(:spree_prices, :updated_at)
   end
 end
